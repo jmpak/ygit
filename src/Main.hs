922582898ce3a814012266f0f2a37515dd6cd2ba
Manuel Paul Joseph <jmpak80@gmail.com>
2015-05-19 19:37:16 +1000

    moved count-objects into its own module

--- src/Main.hs
+++ src/Main.hs
@@ -1,12 +1,7 @@
 import System.Environment
 import System.Directory
-import Data.List
 import System.FilePath.Posix
-import qualified System.FilePath.Find as Find
-import System.FilePath.Find hiding (find)
-import Control.Monad
-import Data.Functor
-import qualified System.Posix.Files as Files
+import Ygit.CountObjects
 
 main :: IO ()
 main = do
@@ -17,29 +12,9 @@ main = do
   case directoryExist of
     True -> 
       case args of
-        ("count-objects":as) -> countObjects gitObjectsDir >>= print
+        ("count-objects":cmdArgs) -> countObjects gitObjectsDir >>= print
+        -- ("hash-object":cmdArgs) -> hashObject cmdArgs gitObjectsDir >>= print
         _ -> print "Yet to be implemented"
     False -> 
       print "fatal: Not a git repository (or any of the parent directories): .git"
-   
-data CountObjects = CountObjects Int Int deriving (Eq)
-instance Show CountObjects where
-  show (CountObjects count size) = 
-    unwords [show count, "objects,", show size, "kilobytes"]
 
-countObjects :: FilePath -> IO CountObjects
-countObjects gitDir = do
-      files <- getAllObjects gitDir
-      size <- sizeOf files
-      return (CountObjects (length files) size)
-
-getAllObjects :: FilePath -> IO [FilePath]
-getAllObjects dir = Find.find (return True) (fileType ==? RegularFile &&? extension /=? ".idx" &&? extension /=? ".pack" &&? fileName /=? "packs") dir
-
-sizeOf :: [FilePath] -> IO Int
-sizeOf files = do
-    sizes <- mapM sizeFor files
-    return $ foldl (+) 0 sizes
-
-sizeFor :: FilePath -> IO Int
-sizeFor file = (fromIntegral . toInteger . Files.fileSize) <$> (Files.getFileStatus file) 
