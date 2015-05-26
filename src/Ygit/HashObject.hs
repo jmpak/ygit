9a7bc96ab505c20a7e73cc43d3edfc02e8509b0e
Svetlana Filimonova <sfilimonova@me.com>
2015-05-26 20:33:34 +1000

    implemented hash objects

--- src/Ygit/HashObject.hs
+++ src/Ygit/HashObject.hs
@@ -4,10 +4,29 @@ module Ygit.HashObject (
 
 import Control.Monad
 import Data.Functor
+import Crypto.Hash.SHA1 (hash)
+import qualified Data.ByteString as Strict
+import qualified Data.ByteString.Char8 as Char8
+import Text.Printf (printf)
+import qualified System.Posix.Files as Files
 
 hashObject :: [String] -> IO ()
 hashObject = mapM_ (hashOfFile >=> print)
 
-hashOfFile :: String -> IO String
-hashOfFile _ = return "asdfadsf"
+hashOfFile :: FilePath -> IO String
+hashOfFile = (fmap toHex) . hashFile
 
+header :: FilePath -> IO Strict.ByteString
+header =  (fmap (\f -> Char8.pack $ "blob " ++ show f ++ "\0" )) . sizeFor
+
+toHex :: Strict.ByteString -> String
+toHex bytes = Strict.unpack bytes >>= printf "%02x"
+
+hashFile :: FilePath -> IO Strict.ByteString
+hashFile f = do
+        fileHeader <- header f
+        fileContent <- Strict.readFile f
+        return $ hash $ (Strict.append fileHeader fileContent)
+
+sizeFor :: FilePath -> IO Int
+sizeFor file = (fromIntegral . toInteger . Files.fileSize) <$> (Files.getFileStatus file)
