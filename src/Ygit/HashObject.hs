0d6bfd328ed0864ab05e58320ea37cd50dae9ead
Manuel Paul Joseph <jmpak80@gmail.com>
2015-06-03 22:38:10 +1000

    implemented hash-object -w

--- src/Ygit/HashObject.hs
+++ src/Ygit/HashObject.hs
@@ -5,32 +5,46 @@ module Ygit.HashObject (
 import Control.Monad
 import Data.Functor
 import Crypto.Hash.SHA1 (hash)
-import qualified Data.ByteString as ByteString
-import qualified Data.ByteString.Char8 as Char8
+import qualified Data.ByteString.Lazy as L
+import Data.ByteString.Lazy (fromChunks)
+import qualified Data.ByteString.Char8 as C
 import Text.Printf (printf)
-import qualified System.Posix.Files as Files
+import qualified System.Posix.Files as F
+import System.Environment
+import System.Directory
+import System.FilePath.Posix
+import qualified Codec.Compression.Zlib as Z
 
 hashObject :: [String] -> IO ()
 hashObject ("-w":files) = mapM_ (hashFile >=> saveAndPrint) files
 hashObject files = mapM_ (hashFile >=> print . fst) files
 
-saveAndPrint :: (ByteString.ByteString, String) -> IO ()
+saveAndPrint :: (C.ByteString, String) -> IO ()
 saveAndPrint (content, h) = do
-  ByteString.writeFile "~/Desktop/test" content
+  cwd <- getCurrentDirectory
+  let dir = (cwd </> ".git/objects" </> (take 2 h))
+  createDirectoryIfMissing True dir
+  let filepath = dir </> (drop 2 h)
+  exists <- doesFileExist filepath
+  if not exists
+  then 
+    L.writeFile filepath $ (Z.compress . fromChunks) [content]
+  else 
+    return ()
   print h
 
-header :: FilePath -> IO ByteString.ByteString
-header =  (fmap (\f -> Char8.pack $ "blob " ++ show f ++ "\0" )) . sizeFor
+header :: FilePath -> IO C.ByteString
+header =  (fmap (\f -> C.pack $ "blob " ++ show f ++ "\0" )) . sizeFor
 
-toHex :: ByteString.ByteString -> String
-toHex bytes = ByteString.unpack bytes >>= printf "%02x"
+toHex :: C.ByteString -> String
+toHex bytes = C.unpack bytes >>= printf "%02x"
 
-hashFile :: FilePath -> IO (ByteString.ByteString, String)
+hashFile :: FilePath -> IO (C.ByteString, String)
 hashFile f = do
         fileHeader <- header f
-        fileContent <- ByteString.readFile f
-        let blobContent = ByteString.append fileHeader fileContent
+        fileContent <- C.readFile f
+        let blobContent = C.append fileHeader fileContent
         return $ (blobContent, toHex $ hash blobContent)
 
 sizeFor :: FilePath -> IO Int
-sizeFor file = (fromIntegral . toInteger . Files.fileSize) <$> (Files.getFileStatus file)
+sizeFor file = (fromIntegral . toInteger . F.fileSize) <$> (F.getFileStatus file)
