eea32de6d48260383de2b5a53fc443288970fc24
Svetlana Filimonova <sfilimonova@me.com>
2015-09-01 20:37:10 +1000

    Parse DIRC from index file

--- src/Ygit/LsFiles.hs
+++ src/Ygit/LsFiles.hs
@@ -2,5 +2,27 @@ module Ygit.LsFiles (
   lsFiles
 ) where
 
+import qualified Data.ByteString.Lazy as BS
+import System.Directory
+import System.FilePath.Posix
+import Data.Binary.Get
+import Data.Word
+
 lsFiles :: [String] -> IO ()
-lsFiles args = print args
+lsFiles args = do 
+      indexFileContent <- indexFile
+      print $ runGet readHeader indexFileContent
+
+indexFile :: IO BS.ByteString
+indexFile = do
+  cwd <- getCurrentDirectory
+  let filePath = (cwd </> ".git" </> "index")
+  BS.readFile filePath
+
+readHeader :: Get BS.ByteString
+readHeader = do 
+  d <- getWord8
+  i <- getWord8
+  r <- getWord8
+  c <- getWord8
+  return $ BS.pack [d,i,r,c]
